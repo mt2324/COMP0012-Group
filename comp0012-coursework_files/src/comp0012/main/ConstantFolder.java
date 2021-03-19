9459a74621b3eb34bbf8aea109c9c7c37443419e
zcabbli@ucl.ac.uk <Rtyugvc231>
2021-03-19 16:35:23 +0000

    m

---
+++ comp0012-coursework_files/src/comp0012/main/ConstantFolder.java
@@ -0,0 +1,72 @@
+package comp0012.main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.ClassGen;
+import org.apache.bcel.classfile.ConstantPool;
+import org.apache.bcel.generic.ConstantPoolGen;
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.InstructionList;
+import org.apache.bcel.util.InstructionFinder;
+import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.TargetLostException;
+
+
+
+public class ConstantFolder
+{
+	ClassParser parser = null;
+	ClassGen gen = null;
+
+	JavaClass original = null;
+	JavaClass optimized = null;
+
+	public ConstantFolder(String classFilePath)
+	{
+		try{
+			this.parser = new ClassParser(classFilePath);
+			this.original = this.parser.parse();
+			this.gen = new ClassGen(this.original);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void optimize()
+	{
+		ClassGen classGen = new ClassGen(original);
+		ConstantPoolGen constPoolGen = classGen.getConstantPool();
+		Method[] methods = classGen.getMethods();
+		for (Method method : methods) {
+			MethodGen methodGen = new MethodGen(method, classGen.getClassName(), constPoolGen);
+			System.out.println(classGen.getClassName() + " > " + method.getName());
+			System.out.println(methodGen.getInstructionList());
+		}
+		this.optimized = gen.getJavaClass();
+	}
+
+	
+	public void write(String optimisedFilePath)
+	{
+		this.optimize();
+
+		try {
+			FileOutputStream out = new FileOutputStream(new File(optimisedFilePath));
+			this.optimized.dump(out);
+		} catch (FileNotFoundException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
